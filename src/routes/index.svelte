bca84fd39b62081e571c6d5e23487c58e117ac65
Cully Wakelin <cullywakelin@gmail.com>
2021-12-11 21:17:40 -0700

    updates

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,4 @@
 <svelte:head>
-  <title>Temporal Platform Documentation</title>
+  <title>My Svelte Project</title>
 </svelte:head>
-<h1 class="text-4xl text-center my-8">Temporal Platform Documentation</h1>
+<h1 class="text-4xl text-center my-8">My Svelte Project</h1>
